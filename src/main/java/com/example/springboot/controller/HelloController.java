6609fdc2ce6fbf8cddd10e16e0577105784d493f
Mr2277 <837544713@qq.com>
2018-06-04 00:48:12 +0800

    init

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Logger;
@@ -11,4 +14,10 @@ public class HelloController {
     private final Logger logger=Logger.getLogger(getClass().toString());
     @Autowired
     private DiscoveryClient discoveryClient;
+    @RequestMapping(value="/hello",method= RequestMethod.GET)
+    public String index(){
+        ServiceInstance instance=discoveryClient.getLocalServiceInstance();
+        logger.info("/hello,host:"+instance.getHost()+",service_id:"+instance.getServiceId());
+        return "Hello World";
+    }
 }
